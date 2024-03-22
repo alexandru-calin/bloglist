deaa65c8e53ff9a7269917c0f6c882f41474a9cf
alexandru calin <alexandrucalin@proton.me>
2024-03-22 16:58:42 +0200

    Unit tests part 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -10,7 +12,42 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    return (prev.likes > current.likes) ? prev : current;
+  };
+
+  return blogs.reduce(reducer, {});
+};
+
+const mostBlogs = (blogs) => {
+  const reducer = (prev, current) => {
+    return (prev.blogs > current.blogs) ? prev : current;
+  };
+
+  const mapper = (val, key) => {
+    return { author: key, blogs: val };
+  };
+
+  return _.map(_.countBy(blogs, "author"), mapper).reduce(reducer, {});
+};
+
+const mostLikes = (blogs) => {
+  const reducer = (prev, current) => {
+    return (prev.likes > current.likes) ? prev : current;
+  };
+
+  const mapper = (val, key) => {
+    return { author: key, likes: _.sumBy(val, "likes") };
+  };
+
+  return _.map(_.groupBy(blogs, "author"), mapper).reduce(reducer, {});
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
