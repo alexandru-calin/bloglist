f93ecf2102d6c00abbfbd6ee6084bbb03402e008
alexandru calin <alexandrucalin@proton.me>
2024-03-23 18:17:59 +0200

    Async/await tests step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,10 +1,33 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const Blog = require("../models/blog");
 
 const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 24,
+  },
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  }
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = initialBlogs.map(blog => new Blog(blog));
+  const promiseArray = blogObjects.map(blog => blog.save());
+  await Promise.all(promiseArray);
+});
 
 test("blogs are returned as json", async () => {
   await api
@@ -13,9 +36,16 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("correct nubmers of blogs", async () => {
+test("correct number of blogs", async () => {
   const response = await api.get("/api/blogs");
-  assert.strictEqual(response.body.length, 2);
+  assert.strictEqual(response.body.length, initialBlogs.length);
+});
+
+test("blog has id property", async () => {
+  const response = await api.get("/api/blogs");
+  for (const blog of response.body) {
+    assert.strictEqual(blog.hasOwnProperty("id"), true);
+  }
 });
 
 after(async () => {
